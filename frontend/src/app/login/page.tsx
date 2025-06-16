3fc4838cfa9e4d95286e013c136dd98c346f0530
Anoop Pandey <84406235+anooppandey17@users.noreply.github.com>
2025-06-16 07:14:53 +0530

    Update page.tsx

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -25,6 +25,7 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
+      console.log("API BASE:", process.env.NEXT_PUBLIC_API_BASE_URL);
       const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/auth/login/`, {
         method: 'POST',
         headers: {
